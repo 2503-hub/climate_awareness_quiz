e4882166dc69be883a4d0f6749ed70cbbbbdf069
Mariette <leloundoumariette@gmail.com>
2026-02-16 23:04:47 +0100

    new

--- frontend/src/components/api/apiClient.js
+++ frontend/src/components/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api"; // your backend URL
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
